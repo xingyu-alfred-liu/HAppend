a9624b00301c8d5484e05eb2ea32ba5af5c573c2
Alfred Liu <xingyul1@andrew.cmu.edu>
2023-04-29 20:14:12 -0700

    add bandstructure data
    add bandstructure data

--- tmp_test.py
+++ tmp_test.py
@@ -1,22 +1,25 @@
 import json
 import numpy as np
-fname = "VEBKAP"
-with open('data/%s.json' % (fname), "r") as f:
-    data = json.load(f)
-abs_a = np.loadtxt("tmp/%s/4-a-absorption_eh.dat" % (fname))
-abs_b = np.loadtxt("tmp/%s/4-b-absorption_eh.dat" % (fname))
-abs_c = np.loadtxt("tmp/%s/4-c-absorption_eh.dat" % (fname))
-# bands_data = np.loadtxt("tmp/%s/bandstructure.dat" % (fname))
-# with open("tmp/%s/kpoints" % (fname),"r") as f:
-#     kpoints = f.readlines()
-# for i, v in enumerate(kpoints):
-#     kpoints[i] = v.replace("\n", "")
-# for i, v in enumerate(kpoints):
-#     kpoints[i] = v.replace("\t", "  ")
-data["gwbse"]["absorption"]["a"] = abs_a.tolist()
-data["gwbse"]["absorption"]["b"] = abs_b.tolist()
-data["gwbse"]["absorption"]["c"] = abs_c.tolist()
-# data["gwbse"]["bandstructure"]["val"] = bands_data.tolist()
-# data["gwbse"]["bandstructure"]["kpoints"] = kpoints
-with open("data/%s_new.json" % (fname), "w") as f:
-    json.dump(data, f, indent=4)
+import os
+
+for fname in os.listdir("bandstructure_dat"):
+    if "kpoints" in os.listdir("bandstructure_dat/%s" % (fname)):
+        with open('data/%s.json' % (fname), "r") as f:
+            data = json.load(f)
+        # abs_a = np.loadtxt("bandstructure_dat/%s/4-a-absorption_eh.dat" % (fname))
+        # abs_b = np.loadtxt("bandstructure_dat/%s/4-b-absorption_eh.dat" % (fname))
+        # abs_c = np.loadtxt("bandstructure_dat/%s/4-c-absorption_eh.dat" % (fname))
+        bands_data = np.loadtxt("bandstructure_dat/%s/bandstructure.dat" % (fname))
+        with open("bandstructure_dat/%s/kpoints" % (fname),"r") as f:
+            kpoints = f.readlines()
+        for i, v in enumerate(kpoints):
+            kpoints[i] = v.replace("\n", "")
+        for i, v in enumerate(kpoints):
+            kpoints[i] = v.replace("\t", "  ")
+        # data["gwbse"]["absorption"]["a"] = abs_a.tolist()
+        # data["gwbse"]["absorption"]["b"] = abs_b.tolist()
+        # data["gwbse"]["absorption"]["c"] = abs_c.tolist()
+        data["gwbse"]["bandstructure"]["val"] = bands_data.tolist()
+        data["gwbse"]["bandstructure"]["kpoints"] = kpoints
+        with open("bandstructure_dat/%s.json" % (fname), "w") as f:
+            json.dump(data, f, indent=4)
