3397d467bd274eb39ad138c621b91ebb7887fa23
Alfred Liu <xingyul1@andrew.cmu.edu>
2023-09-14 07:59:15 -0700

    adding eigenvalues and single mol geo
    adding eigenvalues and single mol geo

---
+++ data_es/es/VUFHUA/submit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -J relaxation # Job name
+#SBATCH -n 56 # Number of total cores
+#SBATCH -N 1 # Number of nodes
+#SBATCH --mem-per-cpu=1000
+#SBATCH -o j_%j.out # File to which STDOUT will be written %j is the job #
+#SBATCH -p debug
+#SBATCH -t 00:10:00
+
+echo "Job started on `hostname` at `date`"
+mpirun -np 56 /home/xingyu/software/FHI-aims-05062019/aims.180607.scalapack.mpi.x &> aims.out
+echo " "
+echo "Job Ended at `date`"
